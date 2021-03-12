dc9a934444811654655f7669c231758a77e30bfe
Shi Tao Luo <sluo5991@gmail.com>
2021-03-12 15:02:52 -0500

    Added login view controller design

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Override point for customization after application launch.
+
+        // --- Copy this only
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "ys4t09jY6tPSOjofUBUwbFcueArSQUDyEQFrtvFS" // <- UPDATE
+                $0.clientKey = "L4bYBuFK4qyjZXgjHuKpQw1YPdp2nwHt7Sz6jPSf" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
         return true
     }
+    
 
     // MARK: UISceneSession Lifecycle
 
@@ -32,5 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    
+    
 }
 
