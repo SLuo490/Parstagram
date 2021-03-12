5e748fe4ec1ef1a0f7d170776a28b8f616e0a831
Shi Tao Luo <sluo5991@gmail.com>
2021-03-12 14:34:19 -0500

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Shi Tao Luo on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
